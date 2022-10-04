b12bf96525885bfb9f9be830748e4a578e798485
Arjya Sen <114721138+virus3-0@users.noreply.github.com>
2022-10-04 15:59:31 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,48 @@
+//Arithmetic Variable
+let a = 50;
+let b = 25;
+let c1 = a/b;
+let c2 = a+b;
+let c3 = a-b;
+let c4 = a*b;
+let c5 = a%b;
+console.log(c1,c2,c3,c4,c5)
+
+
+//Relation Variable
+let d1 = a<b;
+let d2 = a>b;
+let d3 = a=b;
+let d4 = a<=b;
+let d5 = a>=b;
+let d6 = a|=b;
+console.log(d1,d2,d3,d4,d5,d6)
+
+
+//Logical Variable
+let e1 = a&&b;
+let e2 = a||b;
+let e3 = a|b;
+console.log(e1,e2,e3)
+
+
+//Increment Variable
+let f1 = a++;
+let f2 = b++;
+let f3 = a--;
+let f4 = b--;
+console.log(f1,f2,f3,f4)
+
+
+//Conditional Operator
+let g1 = a>b ? a:b;
+console.log(g1)
+
+
+//bit wise
+let h1 = a>>b;
+let h2 = a<<b;
+let h3 = a^b;
+let h4 = a??b;
+let h5 = a&b;
+console.log(h1,h2,h3,h4,h5)
